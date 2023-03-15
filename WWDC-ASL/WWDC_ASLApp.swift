79efb34d73abf7300e6dc610e155cc9afd503e9a
iwb-mouzzam-tahir <mouzzam.tahir@immoweb.be>
2023-03-15 22:41:07 +0100

    Adding Project

---
+++ WWDC-ASL/WWDC_ASLApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC_ASLApp.swift
+//  WWDC-ASL
+//
+//  Created by Mouzzam Tahir on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WWDC_ASLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
